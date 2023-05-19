2d81e27bab6e5602ee794cdbbe289603d3706b26
Web Commerce <62341588+web-commerce-og@users.noreply.github.com>
2023-05-19 17:28:35 +0200

    [TASK] Update ext_emconf.php to be compatible with gridelements 11 and TYPO3 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,8 +19,8 @@ $EM_CONF[$_EXTKEY] = [
 	'clearCacheOnLoad' => 1,
 	'constraints' => [
 		'depends' => [
-			'typo3' => '10.4.0-10.4.99',
-			'gridelements' => '10.0.0-10.99.99',
+			'typo3' => '10.4.0-11.4.99',
+			'gridelements' => '10.0.0-11.99.99',
 		],
 		'conflicts' => [],
         'suggests' => [],
@@ -29,5 +29,3 @@ $EM_CONF[$_EXTKEY] = [
         'psr-4' => ['Laxap\\BootstrapGrids\\' => 'Classes']
     ],
 ];
-
-?>
